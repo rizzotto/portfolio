a5a53328ef3da159cc6e3973fb56708d2e20ddc1
rizzotto <rizzottogui@gmail.com>
2023-06-22 18:47:11 -0300

    feat: video responsive

--- app/(site)/videos/page.tsx
+++ app/(site)/videos/page.tsx
@@ -8,17 +8,19 @@ export default async function Videos() {
       <h2 className="mt-4 mb-8 text-3xl font-bold text-neutral-700 dark:text-white">
         My Videos
       </h2>
-      {data.items.map((video) => (
-        <iframe
-          key={video.id}
-          width={450}
-          height={280}
-          src={`https://youtube.com/embed/videoseries?list=${video.id}`}
-          allowFullScreen
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          className="rounded-3xl"
-        />
-      ))}
+      <div className="max-[955px]:flex max-[955px]:justify-center max-[955px]:items-center">
+        <div className="grid grid-cols-2 max-[955px]:grid-cols-1 ">
+          {data.items.map((video) => (
+            <iframe
+              key={video.id}
+              src={`https://youtube.com/embed/videoseries?list=${video.id}`}
+              allowFullScreen
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              className="rounded-3xl m-4 max-[955px]:mb-4 max-[955px]:m-0 w-[450px] h-[280px] max-[470px]:w-[350px] max-[470px]:h-[240px] max-[370px]:w-[300px] max-[370px]:h-[200px] max-[320px]:w-[250px] max-[320px]:h-[250px]"
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
