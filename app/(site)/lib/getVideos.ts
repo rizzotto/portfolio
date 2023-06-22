67ad909aaca96cafcd60e3e4c64da617accce34d
rizzotto <rizzottogui@gmail.com>
2023-06-22 20:45:57 -0300

    feat: error handling

--- app/(site)/lib/getVideos.ts
+++ app/(site)/lib/getVideos.ts
@@ -4,14 +4,9 @@ export default async function getVideos(): Promise<Videos> {
   const res = await fetch(
     `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2C%20contentDetails&channelId=UCr2AyBagW7Pf9QYQRoBF_DA&key=${process.env.YT_KEY}`
   );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
 
-  // Recommendation: handle errors
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    // throw new Error("Failed to fetch data");
-    console.log("error");
+    throw new Error("Failed to fetch Videos");
   }
 
   return res.json();
